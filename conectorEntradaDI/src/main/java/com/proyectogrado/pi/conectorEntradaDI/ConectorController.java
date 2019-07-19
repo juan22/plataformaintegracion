62365f7b766c88b756ec02139d8a026e3498535b
juan22 <juan.bonhomme22@gmail.com>
2019-07-18 22:15:17 -0300

    Conector de entrada con idMensjae y idCorrelacion OK

--- conectorEntradaDI/src/main/java/com/proyectogrado/pi/conectorEntradaDI/ConectorController.java
+++ conectorEntradaDI/src/main/java/com/proyectogrado/pi/conectorEntradaDI/ConectorController.java
@@ -2,6 +2,8 @@ package com.proyectogrado.pi.conectorEntradaDI;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import org.jboss.logging.MDC;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,15 +40,19 @@ public class ConectorController {
     public String captureReplyChannel(Message<?> message) {
         MessageHeaders headers = message.getHeaders();
         Object channel = headers.getReplyChannel();
-        String correlationID = Utils.getRandomHexString(20);
+        String correlationID = (String) headers.get("X-B3-TraceId");
+        String idMensaje = (String) headers.get("idmensaje");
+        logger.info("idmensaje: "+idMensaje);
+        MDC.put( "idmensaje", idMensaje);
         logger.info("request: "+message.getPayload().toString());
+        logger.info("idcorrelacion: "+correlationID);
         replyChannelMap.put(correlationID, channel);
         return correlationID;
     }
 
     public Object getReplyChannel(Message<?> message) {
         MessageHeaders headers = message.getHeaders();
-        Object correlationID = headers.get("idMensaje");        
+        Object correlationID = headers.get("idcorrelacion");        
         if (correlationID == null || ! (correlationID instanceof String))
             return null;
 
@@ -88,8 +94,9 @@ public class ConectorController {
         logger.info("orquesta payload: "+payload);
         
 		HttpHeaders h = new HttpHeaders();
-		h.add("idSol",(String) headers.get("idsol"));
-		h.add("idMensaje", (String) headers.get("idMensaje"));
+		h.add("idsol",(String) headers.get("idsol"));
+		h.add("idmensaje", (String) headers.get("idmensaje"));
+		h.add("idcorrelacion", (String) headers.get("idcorrelacion"));
 		HttpEntity<String> request = new HttpEntity<String>(payload, h);
 		ResponseEntity<String> response = restTemplate.exchange(serviceUrl + "/orquestador/ejecutarSolucion", HttpMethod.POST, request, String.class);
 		
